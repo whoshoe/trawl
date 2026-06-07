e534d84eaa01e07d2390f13500bfb575fe0d5593
germondai <germondai@gmail.com>
2026-06-07 09:50:00 +0200

    feat: export tiers package public API

---
+++ packages/tiers/src/index.ts
@@ -0,0 +1,17 @@
+export {
+  detectChallengeType,
+  hasHcaptcha,
+  hasRecaptcha,
+  hasTurnstile,
+  isBlocked,
+  isCloudflarePage,
+  needsJs,
+} from "./detect"
+export type { OrchestratorDeps } from "./orchestrator"
+export { scrape } from "./orchestrator"
+export { clearProxyCache, getNextProxy, getRandomProxy } from "./proxyRotator"
+export { solvePageCaptchas } from "./solvers"
+export { runTier1 } from "./tier1"
+export { runTier2 } from "./tier2"
+export { runTier3 } from "./tier3"
+export { runTier4 } from "./tier4"
